8d104a9c87de36284b923e5f16ad13b1ad431097
Márcio Cardoso <93890653+programcod@users.noreply.github.com>
2021-11-11 19:46:52 -0300

    Add files via upload

---
+++ projetos.py
@@ -0,0 +1,10 @@
+letra = input("Digite uma letra: ")
+
+print("A letra digitada foi: ", letra)
+
+print("Seu valor em decimal é: ", ord(letra))
+
+print("Seu valor em hexadecimal é: ", hex(ord(letra)))
+
+print("Seu valor em binário é: ", bin(ord(letra)))
+

d91e891a7294d59397c07ad8116c52e9d1f0289d
Márcio Cardoso <93890653+programcod@users.noreply.github.com>
2021-11-11 21:59:22 -0300

    Update projetos.py
    Resposta

--- projetos.py
+++ projetos.py
@@ -9,4 +9,5 @@ print("Seu valor em hexadecimal é: ", hex(ord(letra)))
 print("Seu valor em binário é: ", bin(ord(letra)))
 
 #input recebe a letra, salva na variável(letra), ord() traduz a letra em decimal,
-#  hex() traduz o decimal em hexadecimal, bin() traduz o decimal em binário.
+# hex() traduz o decimal em hexadecimal, bin() traduz o decimal em binário,
+# print exibe a mensagem com os resultados.
